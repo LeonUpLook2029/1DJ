0a12b6c2a587e803d4a99848fcdf83c6f9449e65
LeonUpLook <pkcet2017@gmail.com>
2022-09-10 08:35:27 +0300

    start_1

---
+++ Leon_2021/blog/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path("",views.blog_index, name="blog_index"),
+    path("<int:pk>/",views.blog_detail, name="blog_detail"),
+]
